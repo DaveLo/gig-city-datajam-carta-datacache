edf30cf72fbb4d0a3fb9ff5fa612d8ec95004fd8
Dave Lowther <dave@davelowther.net>
2024-07-27 15:41:49 -0400

    feat: add request to carta

--- packages/api/main.py
+++ packages/api/main.py
@@ -1,3 +1,5 @@
+from os import environ
+import requests
 from aws_lambda_powertools import Logger, Tracer
 from aws_lambda_powertools.event_handler import APIGatewayHttpResolver
 from aws_lambda_powertools.utilities.typing.lambda_context import LambdaContext
@@ -7,10 +9,14 @@ tracer = Tracer()
 logger = Logger()
 app = APIGatewayHttpResolver()
 
+baseUrl = environ.get('CARTA_URL', 'https://localhost:3000');
+
 @app.get("/routes/<route_id>/buses")
 @tracer.capture_method
 def get_buses_by_route_id(route_id: str):
-    return {"route_id": route_id}
+    url = f"{baseUrl}/bustime/map/getBusesForRoute.jsp?route={route_id}"
+    response = requests.get(url)
+    return {"route_id": route_id, "response": response.text()}
 
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_HTTP)
 @tracer.capture_lambda_handler
